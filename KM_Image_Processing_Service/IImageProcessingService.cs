94e3b7f622ade82037620b68a8af72eb89d34956
Semir <semir91rm@gmail.com>
2022-03-15 02:05:21 +0100

    added comments to service

--- KM_Image_Processing_Service/IImageProcessingService.cs
+++ KM_Image_Processing_Service/IImageProcessingService.cs
@@ -6,6 +6,15 @@ namespace KM_Image_Processing_Service
     [ServiceContract]
     public interface IImageProcessingService
     {
+        /// <summary>
+        /// Service  processing a image in the particular options.
+        /// </summary>
+        /// <param name="imgStream">The image stream to be processed.</param>
+        /// <param name="isHorizontalFlip">If true image will be flipped horizontally.</param>
+        /// <param name="isVerticalFlip">If true image will be flipped vertically.</param>
+        /// <param name="isGrayScale">If true gray scale filter will be applied to the image.</param>
+        /// <param name="isEntropyCrop">If true entropy crop will be applied to the image.</param>
+        /// <returns>The image stream of the processed image.</returns>
         [OperationContract]
         byte[] ProcessImage(byte[] imgStream, bool? isHorizontalFlip,
                                bool? isVerticalFlip, bool? isGrayScale,
